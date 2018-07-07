03353f23cf3ac35f80e68d1d25f55dad7ccb48f2
karthikgotrala <karthikg85@hotmail.com>
2018-07-07 14:38:07 -0500

    Updated demo examples layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'ng-web-accessibility';
 
   options=[
     {"code":"1","label":"Yellow"},
