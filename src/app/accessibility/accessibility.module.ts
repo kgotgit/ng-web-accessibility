216c81b2769d4bcc808d8ba49d47817ba5422e4f
karthikgotrala <karthikg85@hotmail.com>
2019-04-06 18:20:07 -0500

    removed unnecessary files

--- src/app/accessibility/accessibility.module.ts
+++ src/app/accessibility/accessibility.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComboboxComponent } from './combobox/combobox.component';
-import { ListboxComponent } from './listbox/listbox.component';
-import { SingleSelectListboxComponent } from './listbox/single-select-listbox/single-select-listbox.component';
-import { MultiSelectListboxComponent } from './listbox/multi-select-listbox/multi-select-listbox.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ComboboxComponent, ListboxComponent, SingleSelectListboxComponent, MultiSelectListboxComponent],
-  exports:[ComboboxComponent,ListboxComponent, SingleSelectListboxComponent, MultiSelectListboxComponent]
+  declarations: [ComboboxComponent],
+  exports:[ComboboxComponent]
 })
 export class AccessibilityModule { }
