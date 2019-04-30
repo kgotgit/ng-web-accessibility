debb6df70eb1157ed5d1095b053fd2caa21262eb
kgotgit <6868408+kgotgit@users.noreply.github.com>
2019-04-30 11:30:27 -0500

    changed imges folder

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -11,11 +11,11 @@ import { CommonService } from '../services/common.service';
 export class HeaderComponent implements OnInit,OnDestroy  {
 
 
-  private gitLogo=require("../../../assets/images/github-logo.png")
+  
   constructor(private commonService:CommonService) { }
 
   ngOnInit() {
-    console.log(this.gitLogo);
+  
   }
 
   toggleSideNav($event:any){
