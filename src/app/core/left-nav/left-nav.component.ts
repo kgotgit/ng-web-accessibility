621accc479b94b7af09e3f210db6a675b4f87d88
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2019-05-02 20:52:55 -0500

    changed order of components in left nav

--- src/app/core/left-nav/left-nav.component.ts
+++ src/app/core/left-nav/left-nav.component.ts
@@ -33,11 +33,12 @@ export class LeftNavComponent implements OnInit {
   this.navs.push(new NavModel("/combobox","Combobox"));
   this.navs.push(new NavModel("/transferlist","Transfer List"));
   this.navs.push(new NavModel("/alerts","Alerts"));
+  this.navs.push(new NavModel("/progressbars","Progress Bar"));
   this.navs.push(new NavModel("/listgroup","List Group"));
   this.navs.push(new NavModel("/modalwindow","Modal Window"));
   this.navs.push(new NavModel("/tabs","Nav Tabs"));
   this.navs.push(new NavModel("/tables","Table"));
-  this.navs.push(new NavModel("/progressbars","Progress Bar"));
+ 
 
   }
 
