5e9709abfa54424a86afe3f23d82c744611dad1b
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2019-05-04 13:13:01 -0500

    progressbar improvements

---
+++ src/app/model/progressbar.model.spec.ts
@@ -0,0 +1,7 @@
+import { ProgressbarModel } from './progressbar.model';
+
+describe('ProgressbarModel', () => {
+  it('should create an instance', () => {
+    expect(new ProgressbarModel()).toBeTruthy();
+  });
+});
---
+++ src/app/model/progressbar.model.ts
@@ -0,0 +1,13 @@
+export class ProgressbarModel {
+
+    constructor(
+       /*  public currentValue:number=0,
+        public minValue:number=0,
+        public maxValue:number=100,
+        public showText:boolean,
+         */
+
+
+
+    ){}
+}
